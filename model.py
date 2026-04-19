7039259d093456db2e536cc68a1dfac6d5ffaa4a
spyrosgaroufalis <127393684+spyrosgaroufalis@users.noreply.github.com>
2026-04-19 13:42:13 +0300

    mha

--- model.py
+++ model.py
@@ -2,35 +2,36 @@ import torch
 import torch.nn 
 import math
 
+# d_model: dimension of the embedding vector
 class InputEmbeddings(nn.Module):
-    def __init__(self, dimension_model: int, vocab_size: int):
+    def __init__(self, d_model: int, vocab_size: int):
         super().__init__()
-        self.dimension_model = dimension_model
+        self.d_model = d_model
         self.vocab_size = vocab_size
-        self.embedding = torch.nn.Embedding(vocab_size, dimension_model)
+        self.embedding = torch.nn.Embedding(vocab_size, d_model)
 
     # feed forward
     def forward(self, x):
-        return self.embedding(x) * math.sqrt(self.dimension_model)
+        return self.embedding(x) * math.sqrt(self.d_model)
 
 class PositionalEncoding(nn.Module):
-    def __init__(self, dimension_model: int, seq_len: int, dropout: float) -> None:
+    def __init__(self, d_model: int, seq_len: int, dropout: float) -> None:
         super().__init__()
-        self.dimension_model = dimension_model
+        self.d_model = d_model
         self.seq_len = seq_len
         self.dropout = torch.nn.Dropout(dropout)
 
-        # matrix of (seq_len, dimension_model)
-        pe = torch.zeros(seq_len, dimension_model)
+        # matrix of (seq_len, d_model)
+        pe = torch.zeros(seq_len, d_model)
         # creating the vector (tensor) of shape (seq_len, 1)
         position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, dimension_model, 2).float() * (-math.log(10000.0) / dimension_model))
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
         # sin to even positions
         pe[:, 0::2] = torch.sin(position * div_term)
         # cos to odd positions
         pe[:, 1::2] = torch.cos(position * div_term)
 
-        pe = pe.unsqueeze(0) # (1, seq_len, dimension_model)
+        pe = pe.unsqueeze(0) # (1, seq_len, d_model)
         self.register_buffer('pe', pe)
 
     def forward(self, x):
@@ -68,4 +69,57 @@ class FeedForwardBlock(nn.Module):
     def forward(self, x):
         # (batch, seq_len, d_model) --> (batch, seq_len, d_ff) --> (batch, seq_len, d_model)
         return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
+
+class MultiHeadAttentionBlock(nn.Module):
+
+    def __init__(self, d_model: int, h: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model # Embedding vector size
+        self.h = h # Number of heads
+        # Make sure d_model is divisible by h
+        assert d_model % h == 0, "d_model is not divisible by h"
+
+        self.d_k = d_model // h # Dimension of vector seen by each head
+        self.w_q = nn.Linear(d_model, d_model, bias=False) # Wq
+        self.w_k = nn.Linear(d_model, d_model, bias=False) # Wk
+        self.w_v = nn.Linear(d_model, d_model, bias=False) # Wv
+        self.w_o = nn.Linear(d_model, d_model, bias=False) # Wo
+        self.dropout = nn.Dropout(dropout)
+
+    @staticmethod
+    def attention(query, key, value, mask, dropout: nn.Dropout):
+        d_k = query.shape[-1]
+        # Just apply the formula from the paper
+        # (batch, h, seq_len, d_k) --> (batch, h, seq_len, seq_len)
+        attention_scores = (query @ key.transpose(-2, -1)) / math.sqrt(d_k)
+        if mask is not None:
+            # Write a very low value (indicating -inf) to the positions where mask == 0
+            attention_scores.masked_fill_(mask == 0, -1e9)
+        attention_scores = attention_scores.softmax(dim=-1) # (batch, h, seq_len, seq_len) # Apply softmax
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+        # (batch, h, seq_len, seq_len) --> (batch, h, seq_len, d_k)
+        # return attention scores which can be used for visualization
+        return (attention_scores @ value), attention_scores
+
+    def forward(self, q, k, v, mask):
+        query = self.w_q(q) # (batch, seq_len, d_model) --> (batch, seq_len, d_model)
+        key = self.w_k(k) # (batch, seq_len, d_model) --> (batch, seq_len, d_model)
+        value = self.w_v(v) # (batch, seq_len, d_model) --> (batch, seq_len, d_model)
+
+        # (batch, seq_len, d_model) --> (batch, seq_len, h, d_k) --> (batch, h, seq_len, d_k)
+        query = query.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose(1, 2)
+        key = key.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose(1, 2)
+        value = value.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose(1, 2)
+
+        # Calculate attention
+        x, self.attention_scores = MultiHeadAttentionBlock.attention(query, key, value, mask, self.dropout)
+        
+        # Combine all the heads together
+        # (batch, h, seq_len, d_k) --> (batch, seq_len, h, d_k) --> (batch, seq_len, d_model)
+        x = x.transpose(1, 2).contiguous().view(x.shape[0], -1, self.h * self.d_k)
+
+        # Multiply by Wo
+        # (batch, seq_len, d_model) --> (batch, seq_len, d_model)  
+        return self.w_o(x)
 
