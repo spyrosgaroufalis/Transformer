35257b2a67c2b896344b1193557c48ab3b18c5f8
spyrosgaroufalis <127393684+spyrosgaroufalis@users.noreply.github.com>
2026-04-19 13:32:11 +0300

    fdfrwd

--- model.py
+++ model.py
@@ -37,6 +37,8 @@ class PositionalEncoding(nn.Module):
         x = x + (self.pe[:, :x.size(1), :]).requires_grad_(False) # dont want to learn the tensor
         return self.dropout(x)
     
+
+# make sure values dont explode or vanish, keep them reasonable 
 class LayerNormalization(nn.Module):
 
     def __init__(self, eps:float=10**-6) -> None:
@@ -53,3 +55,17 @@ class LayerNormalization(nn.Module):
         std = x.std(dim = -1, keepdim = True) # (batch, seq_len, 1)
         # eps is to prevent dividing by zero or when std is very small
         return self.alpha * (x - mean) / (std + self.eps) + self.bias
+
+# process info for high level features extraction
+class FeedForwardBlock(nn.Module):
+
+    def __init__(self, d_model: int, d_ff: int, dropout: float) -> None:
+        super().__init__()
+        self.linear_1 = nn.Linear(d_model, d_ff) # w1 and b1, bias by default is true
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_ff, d_model) # w2 and b2
+
+    def forward(self, x):
+        # (batch, seq_len, d_model) --> (batch, seq_len, d_ff) --> (batch, seq_len, d_model)
+        return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
+
