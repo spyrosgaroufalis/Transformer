f95831f38a82ab671f39bfb6239eb4d19557944f
spyrosgaroufalis <127393684+spyrosgaroufalis@users.noreply.github.com>
2026-04-19 13:06:52 +0300

    PE

--- model.py
+++ model.py
@@ -9,6 +9,31 @@ class InputEmbeddings(nn.Module):
         self.vocab_size = vocab_size
         self.embedding = torch.nn.Embedding(vocab_size, dimension_model)
 
+    # feed forward
     def forward(self, x):
         return self.embedding(x) * math.sqrt(self.dimension_model)
 
+class PositionalEncoding(nn.Module):
+    def __init__(self, dimension_model: int, seq_len: int, dropout: float) -> None:
+        super().__init__()
+        self.dimension_model = dimension_model
+        self.seq_len = seq_len
+        self.dropout = torch.nn.Dropout(dropout)
+
+        # matrix of (seq_len, dimension_model)
+        pe = torch.zeros(seq_len, dimension_model)
+        # creating the vector (tensor) of shape (seq_len, 1)
+        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, dimension_model, 2).float() * (-math.log(10000.0) / dimension_model))
+        # sin to even positions
+        pe[:, 0::2] = torch.sin(position * div_term)
+        # cos to odd positions
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        pe = pe.unsqueeze(0) # (1, seq_len, dimension_model)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + (self.pe[:, :x.size(1), :]).requires_grad_(False) # dont want to learn the tensor
+        return self.dropout(x)
+    
